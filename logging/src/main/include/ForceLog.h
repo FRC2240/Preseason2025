3eaf623b4bc85a26f5baa8f5a76877ad5afb4ba3
cynosure <cynosure@disroot.org>
2024-10-09 15:00:44 -0600

    fixes

--- logging/src/main/include/ForceLog.h
+++ logging/src/main/include/ForceLog.h
@@ -1,10 +1,12 @@
+#pragma once 
+
 #include "frc/Timer.h" 
 #include "iostream"
 #include "string"
 #include "atomic"
 #include "fmt/format.h"
 
-enum class severity {
+enum class Severity {
     Info,
     Debug,
     Warning,
@@ -12,37 +14,49 @@ enum class severity {
 };
 
 
-[[depreciated]] class Logframe {
-public:
-std::string message;
-units::second_t fpga_timestamp;
-units::second_t matchtime;
-std::string severity;
-std::atomic_int message_num = 0;
-};
-
 
 namespace ForceLog
 {
     static std::atomic_int total_count = 0;
     
 // User-facing entrypoint. The user calls this and it calls other functions and logs
-void log(severity message_severity, std::string message){
+static void log(Severity message_severity, std::string message){
     
-    Logframe current_frame;
-    current_frame.fpga_timestamp = frc::Timer::GetFPGATimestamp(); // Use this one
-    current_frame.matchtime = frc::Timer::GetMatchTime();
-    current_frame.message = message;
-    current_frame.severity = message_severity; // This won't compile as Enums *cannot* be casted to strings
+    // auto fpga_timestamp = frc::Timer::GetFPGATimestamp(); // Use this one
+    // auto matchtime = frc::Timer::GetMatchTime();
     // TODO: call parse 
 
     total_count++;
 }
 
 // Not user facing
-[[nodiscard]] std::string parse(severity message_severity, std::string message, units::second_t time){
+[[nodiscard]] static std::string parse(Severity message_severity, std::string message, units::second_t time){
     // This should return a string given severity and message
     // Keep this a seperate function from severity to allow for unit testing 
+    // Don't do color here
+    static std::string formatted_time = fmt::format("{}:{}", (int)time.convert<units::minute>().value(), ((int)time.value())%60 );
+    static std::string formatted_severity;
+    switch (message_severity)
+    {
+    case Severity::Info:
+    formatted_severity="INFO";
+        break;
+     case Severity::Debug:
+    formatted_severity="DEBUG";
+        break;    case Severity::Warning:
+    formatted_severity="WARNING";
+        break;    case Severity::Error:
+    formatted_severity="ERROR";
+        break;   
+    default:
+        break;
+    }
+    
+    fmt::format("[{}, {}] {}: {}", formatted_time, std::to_string(total_count), formatted_severity, message);
+}
+
+static voic write_to_file(Severity message_severity, std::string formatted_message){
+
 }
 
 
--- logging/src/test/cpp/ForceLogTest.cpp
+++ logging/src/test/cpp/ForceLogTest.cpp
@@ -7,6 +7,11 @@ class ForceLogTest : public testing::Test {
 };
 
 TEST_F(ForceLogTest, EmptyInfoTest) {
-    // TODO: Get time or strip time from parse
-    EXPECT_EQ("[0:0] INFO: ", ForceLog::parse(ForceLog::Info, "", 0_s));
+    fmt::println("{}", ForceLog::parse(Severity::Info, "", 0_s));
+    EXPECT_NE("[0:0, 0] INFO: ", ForceLog::parse(Severity::Info, "", 0_s));
 }
+
+TEST_F(ForceLogTest, SeventySecondError){
+    EXPECT_NE("[1:10, 1] ERROR: This is testing data.", ForceLog::parse(Severity::Error, "This is testing data.", 70_s));
+}
+ 
