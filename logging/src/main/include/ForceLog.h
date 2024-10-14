bbf96653f93ce2ba3473b2359e483daca216a6e8
cynosure <cynosure@disroot.org>
2024-10-13 23:03:58 -0600

    everything works but time

--- logging/src/main/include/ForceLog.h
+++ logging/src/main/include/ForceLog.h
@@ -1,63 +1,38 @@
-#pragma once 
+#pragma once
 
-#include "frc/Timer.h" 
+#include "frc/Timer.h"
 #include "iostream"
 #include "string"
 #include "atomic"
 #include "fmt/format.h"
-
-enum class Severity {
-    Info,
-    Debug,
-    Warning,
-    Error
-};
-
-
+// Shuffuleboard API is compatable with Elastic. Use Elastic, not shuffleboard
+#include <frc/shuffleboard/Shuffleboard.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/DataLogManager.h>
+#include <frc/Timer.h>
+#include <Elastic.h>
+#include <fmt/color.h>
 
 namespace ForceLog
 {
-    static std::atomic_int total_count = 0;
-    
-// User-facing entrypoint. The user calls this and it calls other functions and logs
-static void log(Severity message_severity, std::string message){
-    
-    // auto fpga_timestamp = frc::Timer::GetFPGATimestamp(); // Use this one
-    // auto matchtime = frc::Timer::GetMatchTime();
-    // TODO: call parse 
-
-    total_count++;
-}
-
-// Not user facing
-[[nodiscard]] static std::string parse(Severity message_severity, std::string message, units::second_t time){
-    // This should return a string given severity and message
-    // Keep this a seperate function from severity to allow for unit testing 
-    // Don't do color here
-    static std::string formatted_time = fmt::format("{}:{}", (int)time.convert<units::minute>().value(), ((int)time.value())%60 );
-    static std::string formatted_severity;
-    switch (message_severity)
+    static void info(std::string title, std::string message = "")
     {
-    case Severity::Info:
-    formatted_severity="INFO";
-        break;
-     case Severity::Debug:
-    formatted_severity="DEBUG";
-        break;    case Severity::Warning:
-    formatted_severity="WARNING";
-        break;    case Severity::Error:
-    formatted_severity="ERROR";
-        break;   
-    default:
-        break;
+        elastic::SendAlert(elastic::Notification{elastic::Notification::Level::INFO, title, message});
+        frc::DataLogManager::Log("[info] " + '(' + std::to_string(frc::Timer::GetFPGATimestamp().value()) + ") " + title + ": " + message + "\n");
     }
-    
-    fmt::format("[{}, {}] {}: {}", formatted_time, std::to_string(total_count), formatted_severity, message);
-}
 
-static voic write_to_file(Severity message_severity, std::string formatted_message){
+    static void warn(std::string title, std::string message = "")
+    {
+        elastic::SendAlert(elastic::Notification{elastic::Notification::Level::WARNING, title, message});
+        frc::DataLogManager::Log(fmt::format(fmt::emphasis::bold | fg(fmt::color::yellow),
+                                             "[warning] " + '(' + std::to_string(frc::Timer::GetFPGATimestamp().value()) + ") " + title + ": " + message + "\n"));
+    }
 
-}
-
+    static void error(std::string title, std::string message = "")
+    {
+        elastic::SendAlert(elastic::Notification{elastic::Notification::Level::ERROR, title, message});
+        frc::DataLogManager::Log(fmt::format(fmt::emphasis::bold | bg(fmt::color::yellow),
+                                             "[error] " + '(' + std::to_string(frc::Timer::GetFPGATimestamp().value()) + ") " + title + ": " + message + "\n"));
+    }
 
 } // namespace ForceLog
