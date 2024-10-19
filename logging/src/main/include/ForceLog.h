06ea65de7803deef2c5c2dbb36a9ca5b022e3a37
cynosure <cynosure@disroot.org>
2024-10-19 10:21:56 -0600

    added docs

--- logging/src/main/include/ForceLog.h
+++ logging/src/main/include/ForceLog.h
@@ -15,19 +15,27 @@
 
 namespace ForceLog
 {
+    /// @desc notify the driver about things to know about, but aren't problems
+    /// @param title This is in a slightly larger font in the notification
+    /// @param message the body of the message
     static void info(std::string title, std::string message = "")
     {
         elastic::SendAlert(elastic::Notification{elastic::Notification::Level::INFO, title, message});
         frc::DataLogManager::Log("[info] " + '(' + std::to_string(frc::Timer::GetFPGATimestamp().value()) + ") " + title + ": " + message + "\n");
     }
 
+    /// @desc notify the driver about problems that interfere with robot functionality and preformance, such as not seeing data in expected ranges
+    /// @param title This is in a slightly larger font in the notification
+    /// @param message the body of the message
     static void warn(std::string title, std::string message = "")
     {
         elastic::SendAlert(elastic::Notification{elastic::Notification::Level::WARNING, title, message});
         frc::DataLogManager::Log(fmt::format(fmt::emphasis::bold | fg(fmt::color::yellow),
                                              "[warning] " + '(' + std::to_string(frc::Timer::GetFPGATimestamp().value()) + ") " + title + ": " + message + "\n"));
     }
-
+    /// @desc notify the driver about fatal faults. Only use this if things are very wrong
+    /// @param title This is in a slightly larger font in the notification
+    /// @param message the body of the message
     static void error(std::string title, std::string message = "")
     {
         elastic::SendAlert(elastic::Notification{elastic::Notification::Level::ERROR, title, message});
